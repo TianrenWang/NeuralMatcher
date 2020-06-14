16c2586fc3c3ddcf40afa20814604cd82ff638e3
TianrenWang <broodstar_frank@hotmail.com>
2020-06-13 20:52:49 -0400

    Initial set of changes before debug

--- gc_tpu_run.sh
+++ gc_tpu_run.sh
@@ -2,6 +2,7 @@ JOB_NAME=jobname
 STAGING_BUCKET=gs://neuralsearch
 REGION=us-central1
 DATA_DIR=gs://neuralsearch/raw_data
+DATA_NAME=raw_data
 OUTPUT_PATH=gs://neuralsearch/model
 ENCODED_DATA=gs://neuralsearch/encoded_data
 PACKAGE=gs://neuralsearch/tensorflow-datasets-3.1.0.tar.gz,gs://neuralsearch/nltk-3.5.zip
@@ -17,6 +18,7 @@ gcloud ai-platform jobs submit training $JOB_NAME \
     --packages $PACKAGE \
     -- \
     --data_dir=$DATA_DIR \
+    --data_name=$DATA_NAME \
     --model_dir=$OUTPUT_PATH \
     --encoded_data_dir=$ENCODED_DATA \
     --train_steps=10000 \
@@ -26,7 +28,7 @@ gcloud ai-platform jobs submit training $JOB_NAME \
     --abstract_len=512 \
     --title_len=60 \
     --batch_size=32 \
-    --layers=6 \
+    --layers=4 \
     --depth=512 \
     --feedforward=512 \
     --train=True \
